e5c6f78c41cab83565fb4d3868bb457d83a6d4f0
marloestacx <marloes.tacx@live.nl>
2022-04-28 10:53:30 +0200

    Update keys and dropdown fixed

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -35,3 +35,58 @@ const saveValues = () => {
   localStorage.setItem("arrive", arrive.value);
   localStorage.setItem("departure", departure.value);
 };
+
+//keys
+document.onkeydown = function keyPress(event) {
+  if (event.key == "j" || event.key == "ArrowDown") {
+    const focused = document.querySelector(":focus");
+    if (!focused) {
+      document.querySelector("select:first-of-type").focus();
+    } else {
+      if (focused.id == "departure") {
+        document.getElementById("arrive").focus();
+      } else if (focused.id == "arrive") {
+        document.getElementById("days").focus();
+      } else if (focused.id == "days") {
+        document.getElementById("time").focus();
+      } else if (focused.id == "time") {
+        document.getElementById("save").focus();
+      }
+    }
+  } else if (event.key == "k" || event.key == "ArrowUp") {
+    const focused = document.querySelector(":focus");
+    if (!focused) {
+      document.querySelector("select:first-of-type").focus();
+    } else {
+      if (focused.id == "departure") {
+        console.log("test2");
+        document.getElementById("time").focus();
+      } else if (focused.id == "arrive") {
+        document.getElementById("departure").focus();
+      } else if (focused.id == "days") {
+        document.getElementById("arrive").focus();
+      } else if (focused.id == "time") {
+        {
+          document.getElementById("days").focus();
+        }
+      } else if (focused.id == "save") {
+        document.getElementById("time").focus();
+      }
+    }
+  }
+};
+
+// dropdown
+function changePlaceA() {
+  if (document.getElementById("departure").value == "Amsterdam")
+    document.getElementById("arrive").value = "Eindhoven";
+  else if (document.getElementById("departure").value == "Eindhoven")
+    document.getElementById("arrive").value = "Amsterdam";
+}
+
+function changePlaceB() {
+  if (document.getElementById("arrive").value == "Eindhoven")
+    document.getElementById("departure").value = "Amsterdam";
+  else if (document.getElementById("arrive").value == "Amsterdam")
+    document.getElementById("departure").value = "Eindhoven";
+}
